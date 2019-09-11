bdd3672be1de6f6cdbe6e69e11b24dc64e2e6e05
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-09-11 21:27:36 +0300

    added getProduct method; studied Optional and lambda syntax

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
 import org.slf4j.Logger;
@@ -35,5 +36,14 @@ public class ProductService {
         product.setQuantity(request.getQuantity());
 
         return productRepository.save(product);
+    }
+
+    public Product getProduct(long id) {
+        LOGGER.info("Retrieving product {}", id);
+        // using Optional
+        return productRepository.findById(id)
+                // lambda expressions
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Product " + id + " not found."));
     }
 }
