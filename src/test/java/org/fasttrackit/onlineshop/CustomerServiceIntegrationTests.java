3ac054eb061cc326728c7b963ebf89dac9448bdc
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-09-25 21:30:47 +0300

    added cart service (studied OneToOne relationship)

--- src/test/java/org/fasttrackit/onlineshop/CustomerServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/CustomerServiceIntegrationTests.java
@@ -2,6 +2,7 @@ package org.fasttrackit.onlineshop;
 
 import org.fasttrackit.onlineshop.domain.Customer;
 import org.fasttrackit.onlineshop.service.CustomerService;
+import org.fasttrackit.onlineshop.steps.CustomerSteps;
 import org.fasttrackit.onlineshop.transfer.customer.SaveCustomerRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,18 +22,11 @@ public class CustomerServiceIntegrationTests {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private CustomerSteps customerSteps;
+
     @Test
     public void testCreateCustomer_whenValidRequest_thenReturnCustomer() {
-        SaveCustomerRequest request = new SaveCustomerRequest();
-        request.setFirstName("CustomerFirstName");
-        request.setLastName("CustomerLastName");
-
-        Customer customer = customerService.createCustomer(request);
-
-        assertThat(customer, notNullValue());
-        assertThat(customer.getId(), notNullValue());
-        assertThat(customer.getId(), greaterThan(0L));
-        assertThat(customer.getFirstName(), is(request.getFirstName()));
-        assertThat(customer.getLastName(), is(request.getLastName()));
+        customerSteps.createCustomer();
     }
 }
